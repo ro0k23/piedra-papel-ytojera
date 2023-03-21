64227091c20633e07f4af4d1e96616c291acae6d
ro0k23 <125355361+ro0k23@users.noreply.github.com>
2023-03-21 10:07:55 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,32 @@
 let juego = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    let recivednumber = 0
+    if (recivednumber == 1) {
+        if (juego == 2) {
+            basic.showIcon(IconNames.Happy)
+        } else if (juego == 1) {
+            basic.showIcon(IconNames.Asleep)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
+    } else if (recivednumber == 2) {
+        if (juego == 3) {
+            basic.showIcon(IconNames.Asleep)
+        } else if (juego == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
+    } else if (recivednumber == 3) {
+        if (juego == 2) {
+            basic.showIcon(IconNames.Sad)
+        } else if (juego == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Asleep)
+        }
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     juego = randint(1, 3)
     radio.sendNumber(juego)
